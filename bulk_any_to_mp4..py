1dea8e015df2c4023dc337322b2fcb2e8ab87a8d
Tom Z <78955612+tzim999@users.noreply.github.com>
2023-06-06 17:16:07 -0700

    Update bulk_any_to_mp4.py

--- bulk_any_to_mp4..py
+++ bulk_any_to_mp4..py
@@ -1,12 +1,14 @@
+'''
+Convert all video files in directory to mp4 format. Directory must only contain video files.
+You will have to change the directory location (line 10) and where the ffmpeg.exe is located (line 23)
+12.18.19 Thomas Zimmerman, IBM Research-Almaden
+''
+
 from os import listdir
 from os.path import isfile, join
 import subprocess
 
-#myPath=r'C:\Users\ThomasZimmerman\Videos\SFSU_test\\'
-#myPath=r'C:\Users\ThomasZimmerman\Videos\Aquaponic\Brian\\'
-#myPath=r'C:\Users\ThomasZimmerman\Videos\microscope\Sujoy Annotated\\'
-#myPath=r'C:\Users\ThomasZimmerman\Videos\microscope\Hologram\fewPlankton\\'
-myPath=r'C:\Users\ThomasZimmerman\Videos\microscope\Village Microscope\videoC\\'
+myPath=r'C:\Users\ThomasZimmerman\Videos\microscope\h264_Videos\\'   #directory must contain only video files
 
 files = [f for f in listdir(myPath) if isfile(join(myPath, f))]
 
@@ -18,7 +20,7 @@ for f in files:
         outName=a[0]+'.mp4'
         outFile=myPath+outName
         print ('convert:',f, 'to',outName)
-        subprocess.call([r'C:\Users\ThomasZimmerman\Videos\ffmpeg\\ffmpeg.exe','-i', inFile, '-c','copy', outFile])
+        subprocess.call([r'C:\Users\ThomasZimmerman\Videos\ffmpeg\\ffmpeg.exe','-i', inFile, '-c','copy', outFile])  # this is where the ffmpeg.exe is located
         print ('done')
         print
     else:
